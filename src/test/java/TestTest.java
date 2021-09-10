9abeb188780544cbe7809ee6c60304ebe30fa55b
Max <47674761+Max-first-of@users.noreply.github.com>
2021-09-10 12:33:40 +0400

    new

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,16 @@
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+    @Test
+    @Tag("UI")
+    public void J1(){
+
+    }
+    @Test
+    @Description("Описание")
+    public void J2(){
+
+    }
+}
